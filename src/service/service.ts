ff97e02f41e0cf53dee0b8e61259b398904ce1f1
Dushan Ranasinghage <dushanrandika1994@gmail.com>
2023-06-21 00:28:52 +1200

    Added Apache license

--- src/service/service.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file service.ts
- * @description Copyright service
- * @author Dushan Ranasinghage
- * @copyright Copyright 2023 - Dushan Ranasinghage. All Rights Reserved.
- */
-
-import * as vscode from 'vscode';
-
